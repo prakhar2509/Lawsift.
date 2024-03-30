80ea5f2e4cec76751fbf8acac026c2eee1cf746f
KanishkSogani <kanishksogani1@gmail.com>
2024-03-30 06:39:50 +0530

    Upload pdf created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import Uploadpdf from "./pages/Uploadpdf";
 import { useState } from "react";
 function App() {
   const [data, setData] = useState([]);
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage setData={setData} />} />
+          <Route path="/upload" element={<Uploadpdf setData={setData} />} />
         </Routes>
       </Router>
     </div>
